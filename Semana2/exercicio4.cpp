ae565b9a1d6d7f806182cad15c4a1b8f66780938
Gabriel Teles <gabrielteles0609@gmail.com>
2024-04-14 01:14:50 -0300

    Semana2, exercicio 4

---
+++ Semana2/exercicio4.cpp
@@ -0,0 +1,54 @@
+/*
+    By: Gabriel Teles Medeiros de Aguiar
+    Matrícula: 241008550
+    Curso: Engenharia de Redes de Comunicação - UnB
+    Matéria: Computação Para a Engenharia
+*/
+
+/*    
+    Faça as seguintes declarações na linguagem C++ e imprima os valores de x,y e z
+    x = 7 + 3 x 6 / 2 - 1 
+    y = 2 mod 2 + 2 x 2 / 2
+    z = (3 x 9 (3+(9 x 3/ (3))))
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "* * * * * * * * *";
+    cout << "\n*               *\n*               *\n*               *\n*               *\n*               *\n*               *\n*               *\n";
+    cout << "* * * * * * * * * \n";
+
+    cout << "\n    ***   ";
+    cout << "\n  *     *";
+    cout << "\n *       *";
+    cout << "\n *       *";
+    cout << "\n *       *";
+    cout << "\n *       *";
+    cout << "\n *       *";
+    cout << "\n  *     *";
+    cout << "\n    ***   ";
+
+    cout << "\n\n  *  ";
+    cout << "\n *** ";
+    cout << "\n*****";
+    cout << "\n  *  ";
+    cout << "\n  *  ";
+    cout << "\n  *  ";
+    cout << "\n  *  ";
+    cout << "\n  *  ";
+    cout << "\n  *  ";
+    
+    cout << "\n\n    * ";
+    cout << "\n   * * ";
+    cout << "\n  *   * ";
+    cout << "\n *     * ";
+    cout << "\n*       * ";
+    cout << "\n *     * ";
+    cout << "\n  *   * ";
+    cout << "\n   * * ";
+    cout << "\n    * ";
+
+    return 0;
+}
